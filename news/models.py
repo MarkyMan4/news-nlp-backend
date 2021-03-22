3be5781de7690c276c062f1d4c35993409caa59a
MarkyMan4 <markus.hutnik@gmail.com>
2021-03-21 21:43:40 -0500

    adjusted column definitions

--- news/models.py
+++ news/models.py
@@ -2,10 +2,10 @@ from django.db import models
 
 class Article(models.Model):
     post_id = models.CharField(max_length=10, null=True)
-    post_title = models.CharField(max_length=250)
-    url = models.CharField(max_length=200)
+    post_title = models.CharField(max_length=400)
+    url = models.CharField(max_length=350)
     score = models.IntegerField(null=True)
     publisher = models.CharField(max_length=50, null=True)
-    headline = models.CharField(max_length=250)
+    headline = models.CharField(max_length=400)
     date_published = models.DateTimeField(null=True)
     content = models.CharField(max_length=65000)
