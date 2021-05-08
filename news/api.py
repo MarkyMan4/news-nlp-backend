1a9d3a82ef1d92a555f9210785ee48ae6fbc3b14
MarkyMan4 <markus.hutnik@gmail.com>
2021-05-08 09:41:55 -0500

    added endpoint for article NLP

--- news/api.py
+++ news/api.py
@@ -1,8 +1,8 @@
 from rest_framework import generics, permissions
 from rest_framework import viewsets, status
 from rest_framework.response import Response
-from .serializers import ArticleSerializer
-from .models import Article
+from .serializers import ArticleSerializer, ArticleNlpSerializer
+from .models import Article, ArticleNlp
 
 
 class ArticleViewSet(viewsets.ModelViewSet):
@@ -12,3 +12,9 @@ class ArticleViewSet(viewsets.ModelViewSet):
         article_id = self.request.query_params.get('id')
         return Article.objects.filter(pk=article_id)
 
+class ArticleNlpViewSet(viewsets.ModelViewSet):
+    serializer_class = ArticleNlpSerializer
+
+    def get_queryset(self):
+        article_id = self.request.query_params.get('id')
+        return ArticleNlp.objects.filter(article_id=article_id)
--- news/serializers.py
+++ news/serializers.py
@@ -1,7 +1,12 @@
 from rest_framework import serializers
-from .models import Article
+from .models import Article, ArticleNlp
 
 class ArticleSerializer(serializers.ModelSerializer):
     class Meta:
         model = Article
         fields = ('id', 'post_title', 'url', 'publisher', 'headline', 'date_published', 'content')
+
+class ArticleNlpSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ArticleNlp
+        fields = ('article_id', 'sentiment', 'subjectivity', 'topic')
