9640d95aac9ae611895aacba6c3607a0fa3ae4d4
MarkyMan4 <markus.hutnik@gmail.com>
2021-05-09 21:57:59 -0500

    added query params to ArticleViewSet for filtering results

--- news/api.py
+++ news/api.py
@@ -6,25 +6,32 @@ from .serializers import ArticleSerializer, ArticleNlpSerializer
 from .models import Article, ArticleNlp
 
 
-# class ArticleViewSet(viewsets.ModelViewSet):
-#     serializer_class = ArticleSerializer
-
-#     def get_queryset(self):
-#         article_id = self.request.query_params.get('id')
-#         return Article.objects.filter(pk=article_id)
-
 class ArticleViewSet(viewsets.ViewSet):
     def list(self, request):
         article_queryset = Article.objects.all()
-        # nlp_queryset = ArticleNlp.objects.all()
+
+        query_params = request.query_params
+
+        # any filtering will be provided as query parameters
+        # TODO: figure out how to filter by topic, sentiment and subjectivity since this comes form article_nlp
+        if query_params.get('publisher'):
+            article_queryset = article_queryset.filter(publisher=query_params.get('publisher'))
+
+        # getting a warning here that these are naive datetimes, look into django.utils.datetime
+        if query_params.get('startDate'):
+            article_queryset = article_queryset.filter(date_published__gte=query_params.get('startDate'))
+
+        if query_params.get('endDate'):
+            article_queryset = article_queryset.filter(date_published__lte=query_params.get('endDate'))
+
 
         article_serializer = ArticleSerializer(article_queryset, many=True)
-        # nlp_serializer = ArticleNlpSerializer(nlp_queryset, many=True)
-
         response_data = article_serializer.data
 
         # this seems ineffecient - has to do N queries where N is the number of articles 
-        # in the database. Need to find a better way to do this
+        # in the database. Need to find a better way to do this. Ideally have one query
+        # for articles and one for NLP, then find a way to combine them. Or one query that 
+        # joins article with article_nlp and construct the response myself without serializers
         for article in response_data:
             nlp_queryset = ArticleNlp.objects.filter(article_id=article['id'])
             nlp_serializer = ArticleNlpSerializer(nlp_queryset.first())
@@ -33,6 +40,8 @@ class ArticleViewSet(viewsets.ViewSet):
         return Response(response_data)
 
     def retrieve(self, request, pk=None):
+        if request.query_params:
+            print(request.query_params)
         # queries for article and NLP
         article_queryset = Article.objects.filter(pk=pk)
         nlp_queryset = ArticleNlp.objects.filter(article_id=pk)
