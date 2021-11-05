119be4cc8ca9084631da9f416e2335b1ce475e3a
MarkyMan4 <markus.hutnik@gmail.com>
2021-11-05 18:51:32 -0500

    added end point for getting the probability that user provided text belongs to each of the four topics

--- news/analysis_api.py
+++ news/analysis_api.py
@@ -1,12 +1,16 @@
 from rest_framework import viewsets, status
-from rest_framework import response
 from rest_framework.response import Response
 from rest_framework.decorators import action
+from .models import TopicLkp
 from textblob import TextBlob
 import math
 from textblob import TextBlob
 from nltk.tokenize import word_tokenize, sent_tokenize
 from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+from gensim.models import LdaMulticore
+from backend import settings
+import os
 
 
 class AnalysisView(viewsets.ViewSet):
@@ -49,7 +53,6 @@ class AnalysisView(viewsets.ViewSet):
         return Response({
             'keywords': keywords
         })
-
 
     # Helper methods for getting keywords via TF-IDF
     def get_unique_terms(self, tokens: list) -> list:
@@ -169,4 +172,82 @@ class AnalysisView(viewsets.ViewSet):
         top_ten_terms = [item[1] for item in top_ten]
 
         return top_ten_terms
+
+    @action(methods=['POST'], detail=False)
+    def get_topic_probability(self, request) -> int:
+        """
+        Use the trained LDA model to predict the topic of the given article.
+        This doesn't round the probabilities. That can be handled by the client.
+
+        Args:
+            preprocessed_article (str): article to predict the topic for
+
+        Returns:
+            Response: probabililty of the text being in any one of the topics.
+                      Format:
+                      [
+                        {
+                            "topic_name": <name>,
+                            "probability": <probability>
+                        },
+                        ...
+                      ]
+        """
+        # request body must contain "text"
+        if 'text' not in request.data:
+            return Response({'error': 'must supply text'}, status=status.HTTP_400_BAD_REQUEST)
         
+        # "text" must be a string
+        if type(request.data['text']) != str:
+            return Response({'error': 'text must be a string'}, status=status.HTTP_400_BAD_REQUEST)
+
+        model = LdaMulticore.load(os.path.join(settings.STATIC_ROOT, 'news_lda_model'))
+
+        preprocessed = self.preprocess(request.data['text'])
+
+        # create bag of words with preprocessed article
+        bow = model.id2word.doc2bow(preprocessed)
+
+        # make prediction
+        # pred is a list of tuples of the form (<topic id>, <topic probability>)
+        probabilities = model[bow]
+
+        # Look up the topic name for each result and format for response
+        response = []
+
+        for prob in probabilities:
+            topic_name = TopicLkp.objects.filter(topic_id=prob[0]).first().topic_name
+            probability = prob[1]
+
+            response.append({
+                'topic_name': topic_name,
+                'probability': probability
+            })
+
+        return Response(response)
+
+    def preprocess(self, text: str) -> list:
+        """
+        Preprocess text for topic modeling.
+        This involves removing stop words and any words less than
+        three characters. It also word tokenizes sentences and 
+        lemmatizes each word.
+
+        Args:
+            text (str): Article to preprocess
+
+        Returns:
+            list: list of lowercase, lemmatized tokens from the article
+        """
+        stop_words = stopwords.words('english')
+        lemmatizer = WordNetLemmatizer()
+
+        tokens = word_tokenize(text.lower()) # make all text lower case
+        words = [] # words resulting from applying the filters
+
+        for token in tokens:
+            if len(token) > 3 and token not in stop_words:
+                words.append(lemmatizer.lemmatize(token))
+        
+        return words
+
